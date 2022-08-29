468ef8dd604e4c37329b92415fef69a9768c20bb
jhoemauricio <jhosephmauriciojhoseph@gmail.com>
2022-08-28 21:45:12 -0300

    refatorar listar

--- js/listar.js
+++ js/listar.js
@@ -4,20 +4,38 @@ import URL from './routes.js';
 
 //Exporta a função listar como padrão
 //função recebe o parametro id do usuário
-export default function listar(id){
+export default function listar() {
 
-   
-    //URL concatenada com o id do usuário
-    fetch(URL+id).then(response=>{
+
+    //URL 
+    fetch(URL).then(response => {
 
         //converte a resposta em json
         response.json()
-        
-        //imprime a resposta da requisição
-        .then(data=>console.log(data));
-       
-    }).catch(erro =>{
+
+            //imprime a resposta da requisição
+            .then(data => {
+
+                //Armazena o resultado da resposta, acessando a propriedade data
+                const dados = data.data;
+
+                console.log(dados);
+
+                //Percorre a variavel dados que recebe a data.data
+                Object.keys(dados).forEach(function (item) {
+                    //acessa a propriedade first_name
+                    console.log(dados[item].first_name);
+                });
+
+
+
+            }
+
+            );
+
+    }).catch(erro => {
         console.log(erro);
 
     });
 }
+
